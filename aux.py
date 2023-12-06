a2d4b6be25332b1e1d51cfc8098a24dc1536da52
rtfisher <robert.fisher@umassd.edu>
2023-12-06 22:01:04 +0100

    Added functionality to support inversion call to Helmholtz for isobaric runs.

--- aux.py
+++ aux.py
@@ -1,9 +1,12 @@
 import subprocess
 
-def call_helmholtz(dens, temp, abar, zbar):
+def call_helmholtz(invert, dens, temp, abar, zbar, pres):
     # Construct the command to run the Fortran executable with arguments
-    command = ['_helmholtz/helmholtz.exe', str(dens), str(temp), str(abar), str(zbar)]
-    
+    if not (invert):
+      command = ['_helmholtz/helmholtz.exe', str (invert), str(dens), str(temp), str(abar), str(zbar)] # isochoric
+    else:
+      command = ['_helmholtz/helmholtz.exe', str (invert), str(dens), str(temp), str(abar), str(zbar), str (pres)] # isobaric
+
     # Run the command and capture output
     result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
    
@@ -11,6 +14,6 @@ def call_helmholtz(dens, temp, abar, zbar):
     output_values = result.stdout.split()
     
     # Convert the string values to float and unpack them
-    pres, eint, gammac, gammae, h, cs, cp, cv = map(float, output_values)
+    dens, pres, eint, gammac, gammae, h, cs, cp, cv = map(float, output_values)
     
-    return pres, eint, gammac, gammae, h, cs, cp, cv
+    return dens, pres, eint, gammac, gammae, h, cs, cp, cv
